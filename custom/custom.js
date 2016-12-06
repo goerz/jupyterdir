80339a5d79715591147543a8ee2c8d2dfabcf65f
Michael Goerz <goerz@stanford.edu>
2016-12-06 13:19:58 -0800

    Don't show header and toolbar by default

--- custom/custom.js
+++ custom/custom.js
@@ -1,2 +1,7 @@
 IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
 
+// Hide header and toolbar by default
+$([IPython.events]).on("app_initialized.NotebookApp", function () {
+    $('div#header-container').hide();
+    $('div#maintoolbar').hide();
+});
